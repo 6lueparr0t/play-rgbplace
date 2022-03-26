db119ade7a76c4ff57e614a44475ae42ec51b876
6lueparr0t <6lueparr0t@gmail.com>
2022-03-26 21:59:31 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const debug = process.env.NODE_ENV !== 'production'
+const name = 'play-rgbplace'
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: !debug ? `/${name}/` : '',
+}
