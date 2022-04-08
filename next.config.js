a336df06d0759ff0073a7594297791dd8f43f676
6lueparr0t <6lueparr0t@gmail.com>
2022-04-08 18:06:32 +0900

    update basePath

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== 'production' || false;
 const name = 'play-rgbplace';
 const path = require('path');
 
 module.exports = {
-  basePath: !debug ? `/${name}` : '',
-  assetPrefix: !debug ? `/${name}/` : '',
+  basePath: !debug ? `/${name}` : ``,
+  assetPrefix: !debug ? `/${name}/` : ``,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
